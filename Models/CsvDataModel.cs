e737db0fa23338eed83063c7a3883c62932e2a3e
Chris Stewart <flbas1@gmail.com>
2025-01-17 22:16:33 -0500

    page jumping is Working!!!

--- Models/CsvDataModel.cs
+++ Models/CsvDataModel.cs
@@ -4,5 +4,6 @@ public class CsvDataModel
     public string Value { get; set; }
     public string Section { get; set; }
     public string Notes { get; set; }
+    public string PdfPage { get; set; }
     // Add properties for each column in your CSV file
 }
